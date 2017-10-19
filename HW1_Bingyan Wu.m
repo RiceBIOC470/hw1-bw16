44b7b7b7be3e45a40405f4ffadb28138eb5c2cda
gbri140 <glb3@rice.edu>
2017-10-19 13:49:12 -0500

    Update HW1_Bingyan Wu.m

--- HW1_Bingyan Wu.m
+++ HW1_Bingyan Wu.m
@@ -1,3 +1,17 @@
+GB comments;
+Prob1: 100%
+Prob2:
+P1:100
+P2: 100 
+P3:100
+P4:90
+P5:100
+Prob3
+P1: 100
+P2:100
+P3:100
+Overall: 94
+
 % Homework 1. Due before class on 9/5/17
 
 %% Problem 1 - addition with strings
